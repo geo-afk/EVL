dd135c09dee970cebf8098b426725f995728d05c
geo-afk <geo.a.stewart21@gmail.com>
2026-02-23 14:12:21 -0500

    added config class to dynamically load the environment variables and created setup for structured logging

---
+++ app/utils/log_config.py
@@ -0,0 +1,36 @@
+import sys
+import logging
+import structlog
+
+
+def setup_logging(log_level: str = "INFO"):
+    """
+    Configure structured JSON logging using structlog.
+    """
+    logging.basicConfig(
+        format="%(message)s",
+        stream=sys.stdout,
+        level=getattr(logging, log_level.upper())
+    )
+
+    structlog.configure(
+        processors=[
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+
+            structlog.processors.CallsiteParameterAdder(
+                [
+                    structlog.processors.CallsiteParameter.FILENAME,
+                    structlog.processors.CallsiteParameter.LINENO,
+                    structlog.processors.CallsiteParameter.FUNC_NAME,
+                    structlog.processors.CallsiteParameter.MODULE,
+                ]
+            ),
+
+            structlog.processors.format_exc_info,
+            structlog.processors.JSONRenderer()
+        ],
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
