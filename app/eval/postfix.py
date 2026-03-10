439a6bc6b871b34369ad4dad6f11e604210e26ce
Gizz1111 <gizzie.smalling25@gmail.com>
2026-03-09 22:05:52 -0500

    Implemented infix to postfix

--- app/eval/postfix.py
+++ app/eval/postfix.py
@@ -1,9 +1,61 @@
 
 class Postfix:
 
-    def __init__(self):
-        pass
+    def infix_to_postfix(expression):
+        precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '%': 2}
+        post_fix_output = ""
+        stack = []
 
+        for char in expression:
+            # If an operand
+            if char.isalnum():
+                post_fix_output += char
+            elif char == ' ':
+                continue
+            elif char == '(':
+                stack.append(char)
+            elif char == ')':
+                while stack and stack[-1] != '(':
+                    post_fix_output += stack.pop()
+                stack.pop()
+            # If an operator is found
+            else:
+                while stack and stack[-1] != '(' and precedence[stack[-1]] >= precedence[char]:
+                    post_fix_output += stack.pop()
+                stack.append(char)
 
-    def get_result(self, expression: str) -> float | int:
-        pass
+        # Pop remaining operators
+        while stack:
+            post_fix_output += stack.pop()
+
+        return post_fix_output
+
+    def evaluate_postfix(post_result):
+        stack = []
+
+        for char in post_result:
+            if char == ' ':
+                continue
+            if char.isdigit():
+                stack.append(int(char))
+            else:
+                b = stack.pop()  # right operand
+                a = stack.pop()  # left operand
+
+                if char == '+':
+                    stack.append(a + b)
+                elif char == '-':
+                    stack.append(a - b)
+                elif char == '*':
+                    stack.append(a * b)
+                elif char == '/':
+                    stack.append(a / b)
+                elif char == '%':
+                    stack.append(a % b)
+        return stack[0]
+
+# expression = "( 8 + 2 ) * ( 3 - 1 ) / 4"
+# post_result = infix_to_postfix(expression)
+# result = evaluate_postfix(post_result)
+# print("Postfix:", post_result)
+# print("Result:", result)
